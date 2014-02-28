3b22e737e969c9dd0fed75e400349734f5605c8b
spetschu <spetschu@yahoo.com>
2014-02-28 09:34:35 -0800

    Imported user code snippets

--- singleton.m
+++ singleton.m
@@ -1,3 +1,9 @@
+// singleton
+// 
+//
+// IDECodeSnippetIdentifier: D09B0844-EF20-430B-9A6E-0B92F8A96C59
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
 +(instancetype) shared<#name#> {
     static <#class#> *shared<#name#> = nil;
     static dispatch_once_t onceToken;
